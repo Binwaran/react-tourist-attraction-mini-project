faf9a3b19a4a8fad82415c3e1f889b6956691ec7
Binwaran <binwaran.m@gmail.com>
2025-03-28 16:33:33 +0800

    enable keyword search via input text

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,19 @@ function App() {
   const [text, setText] = useState('') //เอาไว้เก็บข้อมูลที่ค้นหา
   const [places, setPlaces] = useState([]) //เอาไว้เก็บข้อมูลที่เที่ยว
 
- 
 //อันนี้สำหรับดึงข้อมูลที่เที่ยวจาก server
+useEffect(() => {
+  const url = text.trim()==="" ? "http://localhost:4001/trips" : `http://localhost:4001/trips?keywords=${text}`;
+  fetch(url)
+  .then((res)=>res.json())
+  .then((data)=> setPlaces(data.data));
+}, [text]);
+
 
   return (
     <>
-    <SearchSection/>
-    <ShowCard/>
+    <SearchSection text={text} setText={setText} />
+    <ShowCard places={places} setText={setText} text={text}/>
      
     </>
   )
