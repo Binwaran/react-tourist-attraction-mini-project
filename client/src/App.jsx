7a05e9de595b657c963178284667652b1eac72c0
Binwaran <binwaran.m@gmail.com>
2025-03-28 16:08:30 +0800

    create SearchSection component and setup state in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { useState, useEffect } from 'react'
+import './App.css'
+import SearchSection from './components/SearchSection'
+
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  const [text, setText] = useState('') //เอาไว้เก็บข้อมูลที่ค้นหา
+  const [places, setPlaces] = useState([]) //เอาไว้เก็บข้อมูลที่เที่ยว
+
+ 
+//อันนี้สำหรับดึงข้อมูลที่เที่ยวจาก server
+
+  return (
+    <>
+    <SearchSection/>
+     
+    </>
+  )
 }
 
-export default App;
+export default App
